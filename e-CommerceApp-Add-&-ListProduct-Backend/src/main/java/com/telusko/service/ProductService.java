a0ef003a8639399e7b8cdd25bb722968b071e9c5
Gaurav Sharma <gsharma1154@gmail.com>
2024-04-24 19:07:10 +0530

    add and list products

---
+++ e-CommerceApp-Add-&-ListProduct-Backend/src/main/java/com/telusko/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.telusko.service;
+
+import com.telusko.model.Product;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> getAllProducts();
+    Product getProductById(Integer id);
+    Product createProduct(Product product, MultipartFile imageFile) throws Exception;
+
+}
